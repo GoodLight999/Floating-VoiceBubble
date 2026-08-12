4a8c18b4982376762d627e7277f94cf06bb496f4
Hikari <108615941+GoodLight999@users.noreply.github.com>
2026-08-12 12:53:44 +0900

    Use stable Android 36 SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.goodlight.floatingvoicebubble"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.goodlight.floatingvoicebubble"
