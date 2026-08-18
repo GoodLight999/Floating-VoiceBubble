662fa0e2c5a4614845d6aa9205a1d9ee6de6c745
Hikari <108615941+GoodLight999@users.noreply.github.com>
2026-08-19 03:09:25 +0900

    Fix Compose test API import for profile recreation

--- app/src/androidTest/java/com/goodlight/floatingvoicebubble/AppProfileDraftRecreationTest.kt
+++ app/src/androidTest/java/com/goodlight/floatingvoicebubble/AppProfileDraftRecreationTest.kt
@@ -3,7 +3,6 @@ package com.goodlight.floatingvoicebubble
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.hasSetTextAction
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onAllNodes
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
